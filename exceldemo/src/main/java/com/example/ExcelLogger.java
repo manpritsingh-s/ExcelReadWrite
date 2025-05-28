f6165c50ca858325e4ce2aa6755dd7ec1ef6a06d
Panesar <ManpritSingh.Panesar@sony.com>
2025-05-28 12:51:49 +0530

    Sec make changes in the logic

---
+++ exceldemo/src/main/java/com/example/ExcelLogger.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+public class ExcelLogger {
+
+    public static void main(String[] args) {
+        String filePath = "test.xls";
+
+        try (FileInputStream fis = new FileInputStream(filePath); Workbook workbook = new HSSFWorkbook(fis)) {
+
+            Sheet sheet = workbook.getSheetAt(0);
+            int lastRowNum = sheet.getLastRowNum();
+
+            Row newRow = sheet.createRow(lastRowNum + 1);
+            Cell cell = newRow.createCell(0);
+            cell.setCellValue("Build successful!");
+
+            try (FileOutputStream fos = new FileOutputStream(filePath)) {
+                workbook.write(fos);
+            }
+
+            System.out.println("Build message written to Excel file.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
